64fb0df985a237da7738249e062c101c197e25cb
vaecebyZ <vaeceby2@yandex.ru>
2024-01-12 05:32:03 +0800

    Site updated: 2024-01-12 05:31:59

---
+++ precache-manifest.42a05a28398e0ef2ef1155151c80fd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f492d5a489dcade06b4",
+    "url": "/static/css/main.394e7bfc.chunk.css"
+  },
+  {
+    "revision": "8f492d5a489dcade06b4",
+    "url": "/static/js/main.8f492d5a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6a25263f5645b8322dc2",
+    "url": "/static/css/2.5028a386.chunk.css"
+  },
+  {
+    "revision": "6a25263f5645b8322dc2",
+    "url": "/static/js/2.6a25263f.chunk.js"
+  },
+  {
+    "revision": "8ef01e394cc069ffeb2550a0e3c736bc",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.f4cde8148bbd1898716eca3b6440deeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb4c674abcb7188200c",
+    "url": "/static/css/main.394e7bfc.chunk.css"
+  },
+  {
+    "revision": "8eb4c674abcb7188200c",
+    "url": "/static/js/main.8eb4c674.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dffab2c5843818ead15e",
+    "url": "/static/css/2.0e7aef24.chunk.css"
+  },
+  {
+    "revision": "dffab2c5843818ead15e",
+    "url": "/static/js/2.dffab2c5.chunk.js"
+  },
+  {
+    "revision": "c53785a6a7cb93bf596450728d63ff92",
+    "url": "/index.html"
+  }
+];
